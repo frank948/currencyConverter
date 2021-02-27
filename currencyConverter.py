735a138e7eb9aca0b36c40a305d7d84ef1dde267
frank948 <nego94hp@gmail.com>
2021-02-27 18:06:30 -0500

    finished gui???

--- currencyConverter.py
+++
@@ -1,5 +0,0 @@
-import tkinter as tk
-from currencyConverterClass import currency_convert
-
-convert = currency_convert('EUR', 'USD')
-print(convert.base, convert.quote)
--- currencyConverterClass.py
+++ currencyConverterClass.py
@@ -10,20 +10,46 @@ main_w.grid(column=0, row=0, sticky='nsew')
 main_w.columnconfigure(0, weight=1)
 main_w.rowconfigure(0, weight=1)
 
-currencies = []
+currencies = [
+    'USD',
+    'EUR',
+    'CAD'
+]
 
-base = Label(window, text='Base').grid(column=1,row=0,sticky='nw')
-quote = Label(window, text='Quote').grid(column=2,row=0,sticky='nw')
-exchange_rate = Label(window, text='Rate').grid(column=3,row=0,sticky='nw')
+base = StringVar()
+base.set(currencies[0])
+base_options = OptionMenu(window, base, *currencies).grid(column=1, row=1, sticky='nw')
 
-base_ = StringVar()
-quote_ = StringVar()
-rate_ = StringVar()
-base_.set(currencies[0])
-quote_.set(currencies[0])
-base_options = OptionMenu(window, base_, *currencies).grid(column=1,row=1,sticky='nw')
-quote_options = OptionMenu(window, quote_, *currencies).grid(column=2,row=1,sticky='nw')
-rate = Entry(window).grid(column=3,row=1)
+quote = StringVar()
+quote.set(currencies[1])
+quote_options = OptionMenu(window, quote, *currencies).grid(column=2, row=1, sticky='nw')
+
+base_out = StringVar()
+base_out.set('Base1')
+quote_out = StringVar()
+quote_out.set('Quote1')
+
+rate = StringVar()
+rate.set('https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-09-01&symbols={},{} HTTP/1.1'.format(base.get(), quote.get()))
+
+update_base = Label(window, textvariable=base_out).grid(column=3, row=1, sticky='nw')
+update_quote = Label(window, textvariable=quote_out).grid(column=4,row=1,sticky='nw')
+update_rate = Label(window, textvariable=rate).grid(column=5,row=2,sticky='ne')
+
+def get_base(*args):
+    base_out.set(base.get())
+
+def get_quote(*args):
+    quote_out.set(quote.get())
+
+def set_rate(*args):
+    rate.set('https://api.exchangeratesapi.io/history?start_at=2018-01-01&end_at=2018-09-01&symbols={},{} HTTP/1.1'.format(base.get(), quote.get()))
+
+
+quote.trace('w',set_rate)
+base.trace('w', set_rate)
+base.trace('w', get_base)
+quote.trace('w',get_quote)
 
 
 window.mainloop()
